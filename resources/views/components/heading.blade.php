a2877c5b6c8d5f91d44e7aa0ba977a59b778c513
Marlo <111030422+Marlowkey@users.noreply.github.com>
2025-01-09 20:08:35 +0800

    add heading components

--- resources/views/components/heading.blade.php
+++ resources/views/components/heading.blade.php
@@ -2,24 +2,7 @@
     <div class="row">
         <div class="col-12">
             <div class="d-flex justify-content-between align-items-center">
-                <!-- Right side: Heading Text -->
-                <h3 class="mb-0">{{ $headingText }}</h3>
-                <!-- Left side: Button (only visible if $href is set) -->
-                @if($href)
-                    <a class="nav-link active bg-success text-white d-flex align-items-center px-4 py-2 rounded-pill" href="{{ $href }}"
-                        @isset($dataBsToggle) data-bs-toggle="{{ $dataBsToggle }}" @endisset
-                        @isset($dataBsTarget) data-bs-target="{{ $dataBsTarget }}" @endisset>
-                        <i class="{{ $icon }}"></i>
-                        <span>{{ $slot }}</span>
-                    </a>
-                @else
-                    <button class="btn btn-success d-flex align-items-center px-4 py-2 rounded-pill" type="button"
-                        @isset($dataBsToggle) data-bs-toggle="{{ $dataBsToggle }}" @endisset
-                        @isset($dataBsTarget) data-bs-target="{{ $dataBsTarget }}" @endisset>
-                        <i class="{{ $icon }}"></i>
-                        <span>{{ $slot }}</span>
-                    </button>
-                @endif
+                <h3 class="mb-0">{{ $slot }}</h3>
             </div>
         </div>
     </div>
